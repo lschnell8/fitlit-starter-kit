ece0d2dd1b87ce36f8383d7bf6ae435799630ce8
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-07-15 16:47:43 -0500

    Complete hydration.js functionality and pass all tests

--- src/Hydration.js
+++ src/Hydration.js
@@ -5,10 +5,6 @@ class Hydration {
     this.numOunces = userData.numOunces;
   }
 
-
-// For a user (identified by their userID - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
-// For a user, how many fluid ounces they consumed for a specific day (identified by a date)
-// For a user, how many fluid ounces of water consumed each day over the course of a week (7 days) - return the amount for each day
 };
 
 export default Hydration;
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -17,16 +17,32 @@ describe('Hydration', () => {
     "numOunces": 75
   },
   {
-    "userID": 3,
-    "date": "2019/06/15",
+    "userID": 1,
+    "date": "2019/06/18",
     "numOunces": 47
   }];
-      userInfo = userData[1];
-      hydration = new Hydration(userInfo);
-    });
 
-    it('should be a function', () => {
-      expect(Hydration).to.be.a('function');
-    });
-
+  userInfo = userData[1];
+  hydration = new Hydration(userInfo);
   });
+
+  it('should be a function', () => {
+    expect(Hydration).to.be.a('function');
+  });
+
+  it('should be an instance of Hydration', () => {
+    expect(hydration).to.be.an.instanceof(Hydration);
+  });
+
+  it('should indicate the user ID', () => {
+    expect(hydration.id).to.equal(2);
+  });
+
+  it('should indicate the date of the hydration info', () => {
+    expect(hydration.date).to.equal("2019/06/15");
+  });
+
+  it('should indicate the number of ounces drank', () => {
+    expect(hydration.numOunces).to.equal(75);
+  });
+});
