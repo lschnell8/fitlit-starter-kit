87ec6097f102568b44d47a03dcb3a29a7e9bbe38
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-07-15 19:56:31 -0500

    Pass tests on averageWaterPerDay and fluidOzByDate methods

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -3,6 +3,29 @@ class HydrationRepository {
     this.hydrationData = waterInfo;
   }
 
+  averageWaterPerDay(id) {
+    const userOzPerDay = this.hydrationData.filter(user => user.userID === id);
+    // console.log(userOzPerDay);
+    const totalAverage = userOzPerDay.reduce((total, user) => {
+      return total + user.numOunces;
+      }, 0)
+    // console.log(totalAverage);
+    return Math.round(totalAverage / userOzPerDay.length);
+  };
+
+  fluidOzByDate(id, date) {
+    const ozDrunkByUser = this.hydrationData.filter(user => user.userID === id)
+    // console.log(ozDrunkByUser);
+    const ozDrunkOnDate = ozDrunkByUser.find(user => {
+      return user.date === date;
+    });
+    // console.log(ozDrunkOnDate);
+    return ozDrunkOnDate.numOunces;
+  };
+
+  ozDrankInWeek(id, date) {
+    
+  };
 };
 
 export default HydrationRepository;
--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -34,7 +34,7 @@ describe('HydrationRepository', () => {
   {
     "userID": 2,
     "date": "2019/03/11",
-    "numOunces": 91
+    "numOunces": 76
   }];
 
   waterData = new HydrationRepository(hydrationData);
@@ -48,16 +48,23 @@ describe('HydrationRepository', () => {
     expect(waterData).to.be.an.instanceof(HydrationRepository);
   });
 
+  it('should be able to store hydration data', () => {
+    expect(waterData.hydrationData).to.equal(hydrationData);
+  });
 
-  // it('should indicate the user ID', () => {
-  //   expect(hydration.id).to.equal(2);
-  // });
-  //
-  // it('should indicate the date of the hydration info', () => {
-  //   expect(hydration.date).to.equal("2019/06/15");
-  // });
-  //
-  // it('should indicate the number of ounces drank', () => {
-  //   expect(hydration.numOunces).to.equal(75);
-  // });
+  it('should be able to return user average hydration', () => {
+    expect(waterData.averageWaterPerDay(2)).to.equal(81);
+  });
+
+  it('should be able to return ounces drank by specific date', () => {
+    expect(waterData.fluidOzByDate(1, '2019/06/16')).to.equal(69);
+  });
+
+  it('should be able to return ounces drank each day over a week starting at specified data', () => {
+    expect(waterData.ozDrankInWeek(2, '2019/06/15')).to.deep.equal({
+      '2019/06/15': 75,
+      '2019/06/16': 91
+    });
+  });
+
 });
